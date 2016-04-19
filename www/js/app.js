152ed6d0dbe69adf3b72b19fcf083396b70f65b8
birkoss <admin@birkoss.com>
2016-04-19 19:18:53 -0400

    Basic structure and skeleton

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,47 @@
 var deckManager = angular.module('deckManager', ['ionic']);
+
+// console.log(deckManager);
+
+/* FACTORIES */
+
+deckManager.factory('Decks', function() {
+  var decks = [];
+
+  return decks;
+});
+
+deckManager.factory('Deck', function() {
+  var deck = {};
+
+  deck.name = '';
+  deck.cards = [];
+
+  return deck;
+});
+
+/* CONTROLLERS */
+
+deckManager.controller('deckListController', function($scope, $ionicSideMenuDelegate, Decks) {
+  // $scope.decks = Decks.list();
+
+  $scope.decks = Decks;
+
+  $scope.createDeck = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.selectDeck = function(deck) {
+
+  };
+});
+//
+deckManager.controller('deckController', function($scope, $ionicSideMenuDelegate) {
+
+  $scope.toggleMenu = function() {
+    $ionicSideMenuDelegate.toggleLeft();
+  };
+
+  $scope.addDeck = function() {
+
+  };
+});
