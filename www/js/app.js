3218bfb7a55ee0b8e404cc2b748f14da425ea0f3
birkoss <admin@birkoss.com>
2016-04-19 20:13:22 -0400

    DecksList factory + localStorage save

--- www/js/app.js
+++ www/js/app.js
@@ -5,18 +5,45 @@ var deckManager = angular.module('deckManager', ['ionic']);
 /* FACTORIES */
 
 deckManager.factory('Decks', function() {
-  var decks = [];
+  var decks = {};
+
+  decks.decks = [];
+  decks.currentDeck = -1;
+
+  decks.select = function(index) {
+    if( index < decks.decks.length ) {
+      decks.currentDeck = index;
+    }
+  };
+
+  decks.add = function(deck) {
+    decks.decks.push(deck);
+  };
+
+  decks.save = function() {
+    window.localStorage['decks'] = angular.toJson(decks.decks);
+    window.localStorage['currentDeck'] = decks.currentDeck;
+  };
+
+  decks.getCurrentDeck = function() {
+    if( decks.currentDeck < decks.decks.length ) {
+      return decks.decks[ decks.currentDeck ];
+    }
+    return null;
+  };
+
+  var savedDecks = window.localStorage['decks'];
+  if( savedDecks ) {
+    decks.decks = angular.fromJson( savedDecks );
+  }
+
+  var currentDeck = window.localStorage['currentDeck'];
+  if( currentDeck ) {
+    console.log('CD: ' + currentDeck);
+    decks.select(currentDeck);
+  }
 
   return decks;
-});
-
-deckManager.factory('Deck', function() {
-  var deck = {};
-
-  deck.name = '';
-  deck.cards = [];
-
-  return deck;
 });
 
 /* CONTROLLERS */
@@ -24,24 +51,36 @@ deckManager.factory('Deck', function() {
 deckManager.controller('deckListController', function($scope, $ionicSideMenuDelegate, Decks) {
   // $scope.decks = Decks.list();
 
-  $scope.decks = Decks;
+  $scope.decksList = Decks;
 
   $scope.createDeck = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
 
-  $scope.selectDeck = function(deck) {
-
+  $scope.selectDeck = function(index) {
+    $scope.decksList.select(index);
+    $scope.decksList.save();
+    $ionicSideMenuDelegate.toggleLeft();
   };
 });
 //
-deckManager.controller('deckController', function($scope, $ionicSideMenuDelegate) {
+deckManager.controller('deckController', function($scope, $ionicSideMenuDelegate, Decks) {
+
+  $scope.decksList = Decks;
+  $scope.deck = $scope.decksList.getCurrentDeck();
 
   $scope.toggleMenu = function() {
     $ionicSideMenuDelegate.toggleLeft();
   };
 
-  $scope.addDeck = function() {
+  $scope.addCard = function() {
 
   };
+
+  // Watch for a deck change
+  $scope.$watch('decksList.currentDeck', function(newValue, oldValue, scope) {
+    if( newValue != oldValue ) {
+      $scope.deck = $scope.decksList.getCurrentDeck();
+    }
+  });
 });
